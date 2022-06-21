770061bc5433313b3dc6535940f6424ed8a458b0
Nitesh <niteshmandloiconstruction@gmail.com>
2022-06-21 13:39:45 +0530

    Added Centralize Exception Handler feature

---
+++ src/main/java/in/ster/ctl/CentralizedExcptionHandler.java
@@ -0,0 +1,24 @@
+package in.ster.ctl;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class CentralizedExcptionHandler {
+
+	//Handle exception in mvc using @Exception Handler
+	
+		@ExceptionHandler(value = NullPointerException.class)
+		public String exceptionHandlerNull() {
+
+			System.out.println("Null pointer Exception Occur");
+			return "abc";
+		}
+
+		@ExceptionHandler(value = NumberFormatException.class)
+		public String exceptionHandlerNumber() {
+
+			System.out.println("NumberFormat Exception Occur");
+			return "about";
+		}
+}
